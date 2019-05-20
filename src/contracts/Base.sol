e79b3ffc02fdd47481aa43bacca01d230b8c6e71
Alankrutha Chandra Tadepalli <alankrutha@Alankruthas-MacBook-Pro.local>
2019-05-20 02:32:48 -0500

    base contract modified to successfully truffle compile

--- src/contracts/Base.sol
+++ src/contracts/Base.sol
@@ -3,19 +3,39 @@ pragma solidity ^0.5.0;
 import "./Token.sol";
 import "openzeppelin-solidity/contracts/math/SafeMath.sol";
 
-contract Chit {
+contract Base {
     using SafeMath for uint;
+    mapping(uint=>address) public takenSubscribers;
+
 
     struct _Chit {
         uint id;
         address foreman;
         address[] subscribers;
+        uint token;
+        uint totalChit;
         uint16 chitPeriod;
         uint256 timeStamp;
-        uint256 timeIntervals;
     }
+    
+    event createChit(uint id, address foreman, address[] subscribers, uint16 chitPeriod, uint256 timeStamp);
+    event transferChit(address foreman, address[] subscribers, uint16 chitPeriod, uint256 timeStamp);
+    event Deposit(address token, address user, uint256 amount, uint256 balance);
+    event Withdraw(address token, address user, uint256 amount, uint256 balance);
 
-    struct 
+    // function balanceOf(address _token, address _user) public view returns (uint256) {
+        // return tokens[_token][_user];
+    // }
 
-    event CreateChit (address token, )
+    /*function conductChit(uint _id, address _foreman, address[] memory _subscribers, uint16 _chitPeriod, uint256 _timeStamp) public {
+        i = subscribers.length();
+        for(i = 0; i < subscibers.length; i ++){
+            require(subscribers[i] != takenSubscribers[i]);
+            depositToken(subscribers[]);
+        }
+
+    }
+    */
+
 }
+    
--- src/contracts/Token.sol
+++ src/contracts/Token.sol
@@ -6,8 +6,8 @@ contract Token {
     using SafeMath for uint;
 
     // Variables
-    string public name = "DApp Token";
-    string public symbol = "DAPP";
+    string public name = "Chitti";
+    string public symbol = "CHI";
     uint256 public decimals = 18;
     uint256 public totalSupply;
     mapping(address => uint256) public balanceOf;
